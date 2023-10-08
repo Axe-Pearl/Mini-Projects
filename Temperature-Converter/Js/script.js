9b506aba2c4fa4dd4c1d1dcc3ccc4171ad3b96cc
Kishan kumar pathak <pathakkishan000@gmail.com>
2023-10-09 00:22:25 +0530

    changes made

---
+++ Temperature-Converter/Js/script.js
@@ -0,0 +1,69 @@
+console.log('Welcome to 🌡️ Temperature Converter');
+
+const tempLoad = () => {
+    let fa = document.getElementById('fa');
+    fa.innerHTML = "&#xf2cb";
+    fa.style.color = "#ffa41b";
+
+    setTimeout(() => {
+        fa.innerHTML = "&#xf2ca;";
+        fa.style.color = "#ffa41b";
+    }, 1000)
+
+    setTimeout(() => {
+        fa.innerHTML = "&#xf2c9;";
+    }, 2000)
+
+    setTimeout(() => {
+        fa.innerHTML = "&#xf2c8;";
+    }, 3000)
+
+    setTimeout(() => {
+        fa.innerHTML = "&#xf2c7;";
+        fa.style.color = "#ff5151";
+    }, 4000)
+}
+
+setInterval(() => {
+    fa.style.color = "#ffa41b";
+    tempLoad();
+}, 5000);
+
+
+tempLoad();
+
+const calculateTemp = () => {
+    const numberTemp = document.getElementById('temp').value;
+    // console.log(numberTemp);
+
+    const tempSelected = document.querySelector('#temp_diff');
+    const valeTemp = temp_diff.options[tempSelected.selectedIndex].value;
+    // console.log(valeTemp);
+
+
+    // Convert temperature from Celcius to Fahrenheit
+    const celTOfah = (cel) => {
+        let fahrenheit = (cel * (9 / 5) + 32);
+        return fahrenheit;
+    }
+
+    // Convert temperature from Fahrenheit to Celsius
+    const fahTOcel = (fehr) => {
+        let celsius = ((fehr - 32) * 5 / 9);
+        return celsius;
+    }
+
+    let result;
+    if (valeTemp == "cel") {
+        result = celTOfah(numberTemp);
+        document.getElementById('resultContainer').innerHTML = `= ${result}°Fahrenheit`;
+    } else {
+        result = fahTOcel(numberTemp);
+        document.getElementById('resultContainer').innerHTML = `= ${result}°Celsius`;
+    }
+
+    setTimeout(() => {
+        window.location.reload();
+    }, 1500);
+}
+
---
+++ Temperature-Converter/README.md
@@ -0,0 +1,18 @@
+<h1 align="center">🌡️ Temperature Converter Application</h1>
+
+## About
+A temperature converter helps convert the temperature between Fahrenheit and Celsius scale. Temperature is measured using a thermometer. While Kelvin (K) is the SI unit of 
+temperature, people generally use Centigrade or Celsius (°C) and Fahrenheit (°F) to measure temperature.
+
+## Features
+In this application, you can convert the temperature from Celsius to Fahrenheit and vice-versa.
+
+## Made with
+|HTML|CSS|Javascript|
+|---|---|---|
+
+## Link
+https://thenewc0der-24.github.io/Temperature__Converter-Application/
+
+---
+<h3 align="center">Hope this application helpful to you !!</h3>
